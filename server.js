da3c97c6fdce28354bd20711286741f98c1e2865
LiHua Anderson <lihua.anderson@gmail.com>
2021-07-01 19:34:10 -0700

    added code for server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout_tracker",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
+
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
+
+app.listen(PORT, function () {
+  console.log(`Now listening on port: ${PORT}`);
+});
